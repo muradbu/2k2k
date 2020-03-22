b05e8956695fbf8f102d57dbdd597322f1b7bf41
MuradBuyukasik <murad_buyukasik@hotmail.com>
2020-03-22 17:10:32 +0100

    Minor code optimization

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,22 +3,16 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import { useMediaQuery } from 'react-responsive';
 import Logo from './Logo';
 
-const Mobile = ({ children }) => {
-    const isMobile = useMediaQuery({ maxWidth: 767 })
-    return isMobile ? children : null
-}
 const Default = ({ children }) => {
     const isNotMobile = useMediaQuery({ minWidth: 768 })
-    return isNotMobile ? children : null
+    return isNotMobile ? children :
+        <Jumbotron className="text-center mb-0 br-0">
+            <Logo width="100px" />
+        </Jumbotron>
 }
 
 const Banner = () => (
     <div>
-        <Mobile>
-            <Jumbotron className="text-center mb-0 br-0">
-                <Logo width="100px" />
-            </Jumbotron>
-        </Mobile>
         <Default>
             <Jumbotron className="text-center header br-0" />
         </Default>
