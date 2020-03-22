b0d5a3d59685639b962ef6b61dfca49edf4daf0c
MuradBuyukasik <murad_buyukasik@hotmail.com>
2020-03-22 17:37:26 +0100

    Undone "optimizations"

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,16 +3,22 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import { useMediaQuery } from 'react-responsive';
 import Logo from './Logo';
 
+const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+    return isMobile ? children : null
+}
 const Default = ({ children }) => {
     const isNotMobile = useMediaQuery({ minWidth: 768 })
-    return isNotMobile ? children :
-        <Jumbotron className="text-center mb-0 br-0">
-            <Logo width="100px" />
-        </Jumbotron>
+    return isNotMobile ? children : null
 }
 
 const Banner = () => (
     <div>
+        <Mobile>
+            <Jumbotron className="text-center mb-0 br-0">
+                <Logo width="100px" />
+            </Jumbotron>
+        </Mobile>
         <Default>
             <Jumbotron className="text-center header br-0" />
         </Default>
